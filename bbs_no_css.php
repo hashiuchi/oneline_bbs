344db21e8b6fdb6536456c4361461ca161c5001b
hashiuchi <1510320368f@kindai.ac.jp>
2018-07-11 10:31:55 +0800

    bbs.phpにbbs_no_css.phpのインサート文移した

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -7,7 +7,6 @@ $dbh = new PDO($dsn, $user, $password);
 $dbh->query('SET NAMES utf8');
 
 //SQL文を実行する
-//空の送信入るのやめたいが、やめれない、、どうして？
 if (!empty($_POST))
 {
   $nickname = $_POST['nickname'];
@@ -15,10 +14,11 @@ if (!empty($_POST))
 
 if(!empty($nickname||$comment))
   {
-    $sql='INSERT INTO `posts`(`nickname`,`comment`) VALUES(?,?)';
+    $sql='INSERT INTO `posts`(`nickname`,`comment`,`created`) VALUES(?,?,?)';
     //インジェクション
     $data[]=$nickname;
     $data[]=$comment;
+    $data[]=date("Y-m-d H:i:s");
 
     $stmt=$dbh->prepare($sql);
     $stmt->execute($data);
@@ -40,6 +40,31 @@ $dbh=null;
       <p><button type="submit" >つぶやく</button></p>
     </form>
     <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
+<?php
+$dsn='mysql:dbname=oneline_bbs;host=localhost';
+$user='root';
+$password='';
+$dbh=new PDO($dsn,$user,$password);
+$dbh->query('SET NAMES utf8');
+
+$sql='SELECT * FROM `posts` ORDER BY created DESC';
+$stmt=$dbh->prepare($sql);
+$stmt->execute();
+
+while (1) {
+  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+  if($rec==false){
+    break;
+  }
+  echo $rec['id'].'<br>';
+  echo $rec['nickname'].'<br>';
+  echo $rec['comment'].'<br>';
+  echo $rec['created'].'<br>';
+
+  $dbh=null;
+
+}
+?>
 
 </body>
 </html>
